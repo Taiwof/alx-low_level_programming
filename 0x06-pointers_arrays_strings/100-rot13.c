2728334511ff9dcaef23175c2c8ec33397a12f19
Taiwo Bukola <122969049+Taiwof@users.noreply.github.com>
2023-03-01 20:53:48 -0800

    function that encodes a string using rot

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -19,7 +19,6 @@ char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 'k', 'l', 'm', 'n', 'o', 'p',
 'q', 'r', 's', 't', 'u', 'v',
 'w', 'x', 'y', 'z'};
-
 char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 'T', 'U', 'V', 'W', 'X', 'Y',
 'Z', 'A', 'B', 'C', 'D', 'E',
@@ -29,7 +28,6 @@ char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 'x', 'y', 'z', 'a', 'b', 'c',
 'd', 'e', 'f', 'g', 'h', 'i',
 'j', 'k', 'l', 'm'};
-
 while (str[indx1])
 {
 for (indx2 = 0; indx2 < 52; indx2++)
